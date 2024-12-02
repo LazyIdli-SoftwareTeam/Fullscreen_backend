85abb07a93fc299d797dc586389743836640bbf7
BT-cod <mtbalraj@gmail.com>
2024-12-02 09:13:48 +0000

    hh

---
+++ file.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const { checkFolder } = require('./folder');
+let reload = false;
+const createFile = async (d, config, fileInfo, key, ioo) => {
+  const path = __dirname + '/files/' + d[config].customId + '/' + key;
+  const folder = await checkFolder(path);
+  const exists = await fs.existsSync(path + '/' + fileInfo.fileInfo.name);
+  if (exists) {
+    return false;
+  }
+  if (folder && !exists) {
+    return true;
+  }
+};
+
+const deleteInstructionVideo = async (kisokId) => {
+  const path = __dirname + '/files/' + kisokId + '/instructionVideo';
+  const exists = await fs.existsSync(path);
+  if (exists) {
+    const files = await fs.readdirSync(path);
+    for (const file of files) {
+      await fs.unlinkSync(path + '/' + file);
+    }
+  }
+};
+
+module.exports.kioskConfig = async (d, config, ioo) => {
+  for (const el of Object.keys(d[config])) {
+    if (
+      el === 'mediaForTopAd' ||
+      el === 'mediaForBottomOffers' ||
+      el === 'instructionVideo'
+    ) {
+      const publishedFiles = [];
+      let tempO = d[config][el];
+      
+      if (!tempO) continue;
+      for (const fileInfo of tempO) {
+        if (fileInfo.archive) continue;
+        const f = await createFile(d, config, fileInfo, el, ioo);
+        if (f) {
+        }
+        publishedFiles.push(fileInfo.fileInfo.name);
+      }
+      const dirPath = __dirname + '/files/' + d[config].customId + '/' + el;
+      const existPath = await fs.existsSync(dirPath);
+      if (!existPath) continue;
+      fs.readdir(dirPath, async (err, files) => {
+        if (err) console.log(err);
+        for (const file of files) {
+          if (!publishedFiles.includes(file)) {
+            await fs.unlinkSync(dirPath + '/' + file);
+          }
+        }
+      });
+    }
+  }
+
+  const nwKiosk = { ...d };
+  for (const el of Object.keys(nwKiosk.kiosk)) {
+    if (
+      el === 'mediaForTopAd' ||
+      el === 'mediaForBottomOffers' ||
+      el === 'instructionVideo'
+    ) {
+      if (!el || !nwKiosk.kiosk[el]) continue;
+      const newTemp = nwKiosk.kiosk[el].map((e) => {
+        return {
+          fileName: e.fileInfo.name,
+          id: e.fileInfo.id,
+          archive: e.fileInfo.archive || false,
+          all: e.fileInfo.all || false,
+        };
+      });
+      nwKiosk.kiosk[el] = newTemp;
+    }
+  }
+
+  await fs.writeFileSync('config.json', JSON.stringify(nwKiosk));
+};
---
+++ index.js
@@ -0,0 +1,115 @@
+const io = require('socket.io-client');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+//const open = require("open");
+let config = require('./config.json');
+const cors = require('cors');
+const { kioskConfig } = require('./file');
+const LMS_BACKEND_LINK = 'http://13.127.235.58:3001/';
+// const LMS_BACKEND_LINK = 'http://localhost:3001/';
+const KIOSK_ID = 'PTK-005';
+var cron = require('node-cron');
+const ss = require('socket.io-stream');
+const { checkFolder } = require('./folder');
+const socket = io(LMS_BACKEND_LINK);
+const server = require('http').createServer(app);
+const ioo = require('socket.io')(server, { cors: { origin: '*' } });
+socket.on('connect', async () => {
+  console.log('asking for files');
+  socket.emit('askFiles', { kioskId: KIOSK_ID });
+  console.log('connected');
+  cron.schedule('*/30 * * * *', () => {
+    if (!socket.connected) return;
+    console.log('asking for files');
+    socket.emit('askFiles', { kioskId: KIOSK_ID });
+  });
+});
+
+socket.on('error', (e) => {
+  console.log(e);
+});
+socket.on('disconnect', (w) => {
+  console.log('socket disconnected', w);
+});
+
+let i = 0;
+ss(socket).on('downloadFile', async function (stream, data) {
+  if (KIOSK_ID != data.kioskId) return;
+  const path = __dirname + '/files/' + data.kioskId + '/' + data.section;
+  const d = new Date();
+  console.log(
+    'Started at hours = ',
+    d.getHours(),
+    ' minutes = ',
+    d.getMinutes(),
+    ' seconds = ',
+    d.getSeconds()
+  );
+  await checkFolder(path);
+  stream.pipe(fs.createWriteStream(path + '/' + data.fileInfo.name));
+  stream.on('error', (e) => {
+    console.log('error occured', e);
+  });
+  stream.on('data', (e) => {
+    console.log('data', i++, 'data.length = ', e.length);
+  });
+
+  stream.on('end', () => {
+    console.log('file upload success');
+    const d = new Date();
+    console.log(
+      'Ended at hours = ',
+      d.getHours(),
+      ' minutes = ',
+      d.getMinutes(),
+      ' seconds = ',
+      d.getSeconds()
+    );
+    socket.emit('fileUploadSuccessful', { fileInfo: data });
+  });
+});
+
+socket.on('getFiles', async (d) => {
+  try {
+    if (d.kiosk.customId != KIOSK_ID) return console.log('incorrect id');
+    console.log('got files', d);
+    await kioskConfig(d, 'kiosk', socket);
+  } catch (e) {
+    console.log(e);
+  }
+});
+app.use(express.json({ limit: '1000mb' }));
+app.use(cors());
+app.use(express.static('files'));
+app.get('/links', async (req, res) => {
+  try {
+    let data = await fs.readFileSync('config.json');
+    config = JSON.parse(data);
+    const nwKiosk = { ...config };
+    nwKiosk.kiosk.mediaForTopAd = nwKiosk.kiosk.mediaForTopAd.filter(
+      (e) => !e.archive
+    );
+    nwKiosk.kiosk.mediaForBottomOffers =
+      nwKiosk.kiosk.mediaForBottomOffers.filter((e) => !e.archive);
+
+    return res.status(200).json({ message: 'Done!', data: nwKiosk });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: 'Some error occurred' });
+  }
+});
+
+socket.on('error', (e) => {
+  console.log(e);
+});
+
+app.listen(3300, () => {
+//	open("http://localhost:3002", {app: { name: "firefox"}});
+  console.log('app is working on port 3300')
+//require('child_process').exec('firefox-esr -kiosk -private-window http://localhost:3002/?kioskId=PTK-005');
+});
+
+server.listen(4700, () => {
+  console.log('socket are working');
+});
